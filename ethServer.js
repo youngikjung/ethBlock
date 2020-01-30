91b5b8ced86b729650e9dfde359a858868e599db
jungyk <new1234@cmesoft.co.kr>
2020-01-30 13:56:07 +0900

    20200131

--- ethServer.js
+++ ethServer.js
@@ -1,15 +1,17 @@
 "use strict";
 
-// [LOAD PACKAGES]
 const express     = require('express');
 const db          = require('./db.js');
+const bodyParser  = require('body-parser');
 const app         = express();
 db;
 
-// [RUN SERVER]
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const server = app.listen(3000, function(){
     console.log('server has started on port 3000');
 });
 
-// [CONFIGURE ROUTER]
+
 const router = require('./router/main')(app);
